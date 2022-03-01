69574ab2cc81cc5c7a74a5c80d1bb6e35a536822
Erutaner <renaissancezj@gmail.com>
2022-03-01 11:44:42 +0800

    add contact.c and contact.h

---
+++ contact.h
@@ -0,0 +1,26 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+#define MAX 1000
+#define MAX_NAME 20
+#define MAX_SEX 5
+#define MAX_TELE 12
+#define MAX_ADDR 30
+#include<stdio.h>
+#include<string.h>
+struct PeoInfo
+{
+	char name[MAX_NAME];
+	int age;
+	char sex[MAX_SEX];
+	char tele[MAX_TELE];
+	char addr[MAX_ADDR];
+};
+//通讯录类型
+struct Contact
+{
+	struct PeoInfo data[MAX];
+	int size;//记录当前已经有的元素个数
+};
+//声明函数
+void InitContact(struct Contact* ps);
+void AddContact(struct Contact* ps);
+void ShowContact(const struct Contact* ps);
